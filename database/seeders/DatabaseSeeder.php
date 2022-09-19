495af59b15327b6f3c80b3241bfe268afca1a3a4
Rizanoor <rizanoorfauzan123@gmail.com>
2022-09-19 14:51:45 +0700

    create jobController store n update, create jobRequest, change field string('location') from int('location') part 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,48 +28,5 @@ class DatabaseSeeder extends Seeder
                 'role' => "ADMIN",
             ]
         );
-
-        // Jobs::create(
-        //     [
-        //         'jobs_title' => 'Fullstack',
-        //         'company_id' => '1',
-        //         'location' => 1,
-        //         'workspace_type' => "WFH",
-        //         'min_salary' => 5000000,
-        //         'max_salary'=> 10000000,
-        //     ]
-        // );
-
-        // Jobs::create(
-        //     [
-        //         'jobs_title' => 'Frontend Dev',
-        //         'company_id' => '2',
-        //         'location' => 2,
-        //         'workspace_type' => "WFH",
-        //         'min_salary' => 5000000,
-        //         'max_salary'=> 10000000,
-        //     ]
-        // );
-        // Jobs::create(
-        //     [
-        //         'jobs_title' => 'Backend Dev',
-        //         'company_id' => '3',
-        //         'location' => 3,
-        //         'workspace_type' => "WFO",
-        //         'min_salary' => 6000000,
-        //         'max_salary'=> 11000000,
-        //     ]
-        // );
-        // Jobs::create(
-        //     [
-        //         'jobs_title' => 'Devops',
-        //         'company_id' => '4',
-        //         'location' => 4,
-        //         'workspace_type' => "WFH",
-        //         'min_salary' => 3000000,
-        //         'max_salary'=> 8000000,
-        //     ]
-        // );
-
     }
 }
